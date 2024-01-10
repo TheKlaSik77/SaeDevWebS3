b278aae483f492ecb57e05ef59bb4fe5d43a15a9
ame02h08 <amelia.butinfo@gmail.com>
2024-01-10 15:12:46 +0100

    commit 10 janvier

--- template.php
+++ template.php
@@ -8,12 +8,13 @@ Initiated by Ismael ARGENCE & Mathéo NGUYEN & Nathan FENOLLOSA -->
 	<meta charset="ISO-8859-1">
 	<meta name="viewport"content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style.css">
-	<title>Après la Pluie</title>
+	<title>Metallic Infestation</title>
     <link rel="icon" type="image/x-icon" href="administration/media/logo.ico">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
+    
     <script
 	  src="https://code.jquery.com/jquery-3.6.1.min.js"
 	  integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="
